7ffb7049632a8d0d6e09048dd2f96f44de23b5df
Hanna Lee <younghyoun.l@gmail.com>
2023-09-08 19:53:18 +0900

    Initial commit

---
+++ src/main/java/com/metavirtual/bloom/controller/MainController.java
@@ -0,0 +1,41 @@
+package com.metavirtual.bloom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+
+
+
+
+
+
+    @GetMapping("/index")
+    public void gomain() {
+    }
+
+    @GetMapping("/")
+    public String defaultLocation() {
+        return "index";
+    }
+
+    @GetMapping("regist")
+    public String regist(){
+        return "regist/registCategory";
+    }
+
+    @GetMapping("test")
+    public String test(){
+
+        return "psychological/firstTest";
+    }
+    @GetMapping("start")
+    public String test1(){
+
+        return "psychological/testStart";
+    }
+}
+/**/
