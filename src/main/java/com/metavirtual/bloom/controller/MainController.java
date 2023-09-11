5b7787dc1346603fe77b157343a326fbac59c2d5
Gray-Grazer <cleit8307@gmail.com>
2023-09-11 09:49:17 +0900

    review css 검색창, 검색버튼 추가 아직 진행중

--- src/main/java/com/metavirtual/bloom/controller/MainController.java
+++ src/main/java/com/metavirtual/bloom/controller/MainController.java
@@ -22,19 +22,16 @@ public class MainController {
 
 
 
-
-
-
-
-
-
-
-
-
     @GetMapping("review")
     public String review() {
         return "reviewboard/reviewmain";
     }
+
+    @GetMapping("paging")
+    public String paging() {
+        return "reviewboard/paging";
+    }
+
 
 
 }
