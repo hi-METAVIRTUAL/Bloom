4fb46ab12a4a81ec03a9ee2af8272d3b649b99a7
jiyeong08 <a01086283142@gmail.com>
2023-09-11 14:55:09 +0900

    20230911 JY userpage finish, therapistpage adminqna ~ing

--- src/main/java/com/metavirtual/bloom/controller/therapist/TherapistController.java
+++ src/main/java/com/metavirtual/bloom/controller/therapist/TherapistController.java
@@ -1,10 +1,30 @@
 package com.metavirtual.bloom.controller.therapist;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/therapist")
 public class TherapistController {
 
+    @GetMapping("/infoSetting")
+    public String infoSetting(){
+        return "mypage/therapist/infoSetting";
+    }
+
+    @GetMapping("/profile")
+    public String profile(){
+        return "mypage/therapist/profile";
+    }
+
+    @GetMapping("/profileSetting")
+    public String profileSetting(){
+        return "mypage/therapist/profileSetting";
+    }
+
+    @GetMapping("/reservManage")
+    public String reservManage(){
+        return "mypage/therapist/reservManage";
+    }
 }
