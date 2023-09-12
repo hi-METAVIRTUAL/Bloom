01ddd97bf10b9cdecda3dfcd9b8105ed9fbfe064
jiyeong08 <a01086283142@gmail.com>
2023-09-12 12:01:26 +0900

    20230912 reservManage+popup, therapistProfile complete

--- src/main/java/com/metavirtual/bloom/controller/therapist/TherapistController.java
+++ src/main/java/com/metavirtual/bloom/controller/therapist/TherapistController.java
@@ -9,22 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TherapistController {
 
     @GetMapping("/infoSetting")
-    public String infoSetting(){
+    public String infoSetting() {
         return "mypage/therapist/infoSetting";
     }
 
     @GetMapping("/profile")
-    public String profile(){
+    public String profile() {
         return "mypage/therapist/profile";
     }
 
     @GetMapping("/profileSetting")
-    public String profileSetting(){
+    public String profileSetting() {
         return "mypage/therapist/profileSetting";
     }
 
     @GetMapping("/reservManage")
-    public String reservManage(){
+    public String reservManage() {
         return "mypage/therapist/reservManage";
+    }
+
+    @GetMapping("/reservPopup")
+    public String reservPopup() {
+        return "mypage/therapist/reservPopup";
     }
 }
