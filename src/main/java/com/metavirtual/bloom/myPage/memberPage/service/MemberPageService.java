88525b9236e5d83d37c7c3fe27b729b59aefdd1b
jiyeong08 <a01086283142@gmail.com>
2023-09-14 21:28:31 +0900

    20230914_JY_member mypage

--- src/main/java/com/metavirtual/bloom/myPage/memberPage/service/MemberPageService.java
+++ src/main/java/com/metavirtual/bloom/myPage/memberPage/service/MemberPageService.java
@@ -1,7 +1,9 @@
 package com.metavirtual.bloom.myPage.memberPage.service;
 
-import com.metavirtual.bloom.user.dto.MemberDTO;
+import com.metavirtual.bloom.user.model.dto.MemberDTO;
 
 public class MemberPageService {
-//    public void modifyMemberInfo(MemberDTO member, UserInfo_DTO userInfo);
+
+//    public void modifyMemberInfo(MemberDTO member, UserDTO user);
+
 }
