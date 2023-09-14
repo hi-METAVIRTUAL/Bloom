1ccadb51bcca3c62db9df3f14902d2049f7019e8
jiyeong08 <a01086283142@gmail.com>
2023-09-14 10:36:18 +0900

    20230914_JY_DTO

---
+++ src/main/java/com/metavirtual/bloom/model/dto/test/ProfileFileDTO.java
@@ -0,0 +1,12 @@
+package com.metavirtual.bloom.model.dto.test;
+
+public class ProfileFileDTO {
+
+    private int fileNumber;
+    private String userId;
+    private String filePath;
+    private String fileOriginName;
+    private String fileChangedName;
+    private int fileSize;
+
+}
