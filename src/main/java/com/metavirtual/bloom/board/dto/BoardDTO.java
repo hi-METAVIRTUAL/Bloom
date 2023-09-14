a61eee0faa4bfaf3dafade1410b74c1cf2581dbb
Gray-Grazer <cleit8307@gmail.com>
2023-09-14 09:47:57 +0900

    board service 생성

--- src/main/java/com/metavirtual/bloom/board/dto/BoardDTO.java
+++ src/main/java/com/metavirtual/bloom/board/dto/BoardDTO.java
@@ -1,4 +1,9 @@
 package com.metavirtual.bloom.board.dto;
 
 public class BoardDTO {
+    private int boardCode; //게시글코드
+    private String title; // 제목
+    private String postedDate; // 게시물 작성일시
+    private String deleteDate; //
+
 }
