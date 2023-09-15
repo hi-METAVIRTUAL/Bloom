6f2c8faab0869bc708174d847ef5df903e5c778a
Gray-Grazer <cleit8307@gmail.com>
2023-09-15 19:03:45 +0900

    제발되라,,, test

--- src/main/java/com/metavirtual/bloom/board/model/service/BoardService.java
+++ src/main/java/com/metavirtual/bloom/board/model/service/BoardService.java
@@ -1,9 +1,13 @@
-/*
+
 package com.metavirtual.bloom.board.model.service;
 
 import com.metavirtual.bloom.board.model.dao.BoardMapper;
 import com.metavirtual.bloom.board.model.dto.BoardDTO;
+import com.metavirtual.bloom.common.exception.board.BoardPostingException;
+import com.metavirtual.bloom.common.paging.SelectCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,12 +16,29 @@ public class BoardService {
 
     private final BoardMapper boardMapper;
 
+    @Autowired
     public BoardService(BoardMapper boardMapper) {
         this.boardMapper = boardMapper;
     }
 
-    public List<BoardDTO> searchAllBoard() {
-        return BoardMapper.searchAllBoard();
+
+    /* 전체 게시글 조회 메서드 */
+    public List<BoardDTO> findAllBoard(SelectCriteria selectCriteria) {
+        List<BoardDTO> boardList = boardMapper.findAllBoard(selectCriteria);
+
+        return boardList;
     }
+
+
+    /* bboardInsert.html 게시글 등록 메서드 */
+    @Transactional
+    public void boardNewPosting(BoardDTO newPosting) throws BoardPostingException {
+        int result = boardMapper.boardNewPosting(newPosting);
+
+        if(!(result > 0)) {
+            throw new BoardPostingException("게시글 등록에 실패하였습니다");
+        }
+    }
+
 }
-*/
+
---
+++ src/main/java/com/metavirtual/bloom/common/exception/board/BoardPostingException.java
@@ -0,0 +1,11 @@
+package com.metavirtual.bloom.common.exception.board;
+
+public class BoardPostingException extends Exception {
+
+
+    public BoardPostingException() {}
+
+    public BoardPostingException(String message) {
+        super(message);
+    }
+}
