7b02d23598f8f2304155a8a8d9e3e5f1ee9a95e0
wjs960 <wjs960@gmail.com>
2023-09-18 01:30:59 +0900

    2023-09-18 001 db 너무어렵다

--- src/main/java/com/metavirtual/bloom/psychometry/controller/PsychometryController.java
+++ src/main/java/com/metavirtual/bloom/psychometry/controller/PsychometryController.java
@@ -2,10 +2,11 @@ package com.metavirtual.bloom.psychometry.controller;
 
 import com.metavirtual.bloom.psychometry.model.dto.TestQDTO;
 import com.metavirtual.bloom.psychometry.model.service.PsychometryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,16 +23,14 @@ public class PsychometryController {
 
     @GetMapping("first")
     public String test(){ return "psychological/psychometry/firstTest"; }
-    @GetMapping("start")
-    public String startTestPage(Model model){
+
+    @GetMapping(value = "start",produces = "application/json; charset=UTF-8")
+    @ResponseBody
+    public List<TestQDTO>  startTestPage() {
 
         List<TestQDTO> testQ = psychometryService.findContent();
-
-        model.addAttribute("testQ", testQ);
-
-        return "psychological/psychometry/startTest";
+        return testQ;
     }
-
     @GetMapping("last")
     public String lastTestPage(){
         return "psychological/psychometry/lastTest";
--- src/main/java/com/metavirtual/bloom/psychometry/model/service/PsychometryService.java
+++ src/main/java/com/metavirtual/bloom/psychometry/model/service/PsychometryService.java
@@ -2,10 +2,12 @@ package com.metavirtual.bloom.psychometry.model.service;
 
 import com.metavirtual.bloom.psychometry.model.dao.PsychometryMapper;
 import com.metavirtual.bloom.psychometry.model.dto.TestQDTO;
+import com.metavirtual.bloom.psychometry.model.dto.TestResultDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PsychometryService {
@@ -18,7 +20,11 @@ public class PsychometryService {
     }
 
     public List<TestQDTO> findContent() {
-
         return psychometryMapper.findContent();
+    }
+
+    public TestQDTO findContentByCategory(String index) {
+        return psychometryMapper.getQuestionByIndex(index);
+
     }
 }
