29566efa1deb32e011affe99015f3f267c1e7518
jiyeong08 <a01086283142@gmail.com>
2023-09-20 22:10:56 +0900

    20230919 MemberMyPage, TherapistMyPage

--- src/main/java/com/metavirtual/bloom/myPage/therapistPage/model/service/TherapistPageServiceImpl.java
+++ src/main/java/com/metavirtual/bloom/myPage/therapistPage/model/service/TherapistPageServiceImpl.java
@@ -2,6 +2,7 @@ package com.metavirtual.bloom.myPage.therapistPage.model.service;
 
 import com.metavirtual.bloom.common.exception.myPage.ModifyInfoException;
 import com.metavirtual.bloom.myPage.therapistPage.model.dao.TherapistPageMapper;
+import com.metavirtual.bloom.myPage.therapistPage.model.dto.ProfileFileDTO;
 import com.metavirtual.bloom.user.model.dto.TherapistDTO;
 import com.metavirtual.bloom.user.model.dto.UserDTO;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,20 @@ public class TherapistPageServiceImpl implements TherapistPageService{
 
     private final TherapistPageMapper mapper;
     public TherapistPageServiceImpl(TherapistPageMapper mapper){this.mapper = mapper;}
+
+    @Override
+    public void uploadProfileImg(ProfileFileDTO profileFileDTO) throws ModifyInfoException{
+        int result;
+
+        if(profileFileDTO.getFileNumber() == 0){
+            result = mapper.uploadPfImg(profileFileDTO);
+        } else {
+            result = mapper.updatePfImg(profileFileDTO);
+        }
+        if(!(result>0)){
+            throw new ModifyInfoException("❌프로필 사진 업로드 실패❌");
+        }
+    }
 
     @Override
     public void modifyTherapistInfo(UserDTO user) throws ModifyInfoException {
@@ -31,8 +46,8 @@ public class TherapistPageServiceImpl implements TherapistPageService{
     }
 
     @Override
-    public void modifyActivationStatus() throws ModifyInfoException{
-        int result = mapper.modifyActivationStatus();
+    public void modifyActivationStatus(char activationStatus) throws ModifyInfoException{
+        int result = mapper.modifyActivationStatus(activationStatus);
 
         if(!(result>0)){
             throw new ModifyInfoException(("❌상담활동 활성화 여부 변경 실패❌"));
