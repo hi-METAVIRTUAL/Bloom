8538dcc99f852e2df1e4c90623ab564ad1dd17c9
Hanna Lee <younghyoun.l@gmail.com>
2023-09-21 12:19:24 +0900

    빠진 파일 pr

---
+++ src/main/java/com/metavirtual/bloom/mailVerification/MailController.java
@@ -0,0 +1,27 @@
+package com.metavirtual.bloom.mailVerification;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class MailController {
+
+    private final MailService mailService;
+
+    @ResponseBody
+    @PostMapping("/mail")
+    public String MailSend(String mail){
+
+        int number = mailService.sendMail(mail);
+
+        String num = "" + number;
+
+        return num;
+    }
+}
