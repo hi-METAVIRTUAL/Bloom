6e95c94714fd7efe51b1f764969c0cbc50f7d199
wjs960 <wjs960@gmail.com>
2023-09-28 10:35:57 +0900

    상담사리스트

--- src/main/java/com/metavirtual/bloom/match/model/dao/MatchMapper.java
+++ src/main/java/com/metavirtual/bloom/match/model/dao/MatchMapper.java
@@ -1,17 +1,20 @@
 package com.metavirtual.bloom.match.model.dao;
 
+import com.metavirtual.bloom.common.paging.SelectCriteria;
 import com.metavirtual.bloom.match.model.dto.TherapistInfoDTO;
 import com.metavirtual.bloom.psychometry.model.dto.MemberTestResultDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface MatchMapper {
-    List<TherapistInfoDTO> findAllTherapist();
+    List<TherapistInfoDTO> findAllTherapist(SelectCriteria selectCriteria);
 
-    List<TherapistInfoDTO> getTotalScore(String userId);
+    List<MemberTestResultDTO> findMemberTest(String userId);
 
     List<TherapistInfoDTO> therapyRecommend();
 
+    int selectTotalCount(Map<String, String> searchMap);
 }
