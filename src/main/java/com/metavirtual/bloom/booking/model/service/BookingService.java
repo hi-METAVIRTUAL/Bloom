92d9dde3432e3cd9686ddf6216dbe690c1f30022
Gray-Grazer <cleit8307@gmail.com>
2023-10-01 17:03:19 +0900

    후기페이지 DB 작성완료, 검색기능 카테고리 추가하여 수정 진행중

--- src/main/java/com/metavirtual/bloom/booking/model/service/BookingService.java
+++ src/main/java/com/metavirtual/bloom/booking/model/service/BookingService.java
@@ -1,4 +1,56 @@
 package com.metavirtual.bloom.booking.model.service;
 
+import com.metavirtual.bloom.booking.model.dao.BookingMapper;
+import com.metavirtual.bloom.booking.model.dto.ReviewDTO;
+import com.metavirtual.bloom.common.exception.booking.ReviewDeleteException;
+import com.metavirtual.bloom.common.exception.booking.ReviewInsertException;
+import com.metavirtual.bloom.common.paging.SelectCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class BookingService {
+
+    private final BookingMapper bookingMapper;
+
+    @Autowired
+    public BookingService (BookingMapper bookingMapper) {
+        this.bookingMapper = bookingMapper;
+    }
+
+    public int selectTotalCount() {
+
+        int result = bookingMapper.selectTotalCount();
+
+        return result;
+    }
+
+    /* 전체 후기 조회 */
+    public List<ReviewDTO> findAllReview(SelectCriteria selectCriteria) {
+        List<ReviewDTO> reviewList = bookingMapper.findAllReview(selectCriteria);
+        return reviewList;
+    }
+
+    /* 후기 등록 */
+    @Transactional
+    public void newReviewPosting(ReviewDTO newReview) throws ReviewInsertException {
+        int result = bookingMapper.newReviewPosting(newReview);
+        if(!(result > 0)) {
+            throw new ReviewInsertException("후기 등록에 실패하였습니다.");
+        }
+    }
+
+    /* 후기 삭제 */
+    @Transactional
+    public void reviewDelete(ReviewDTO deleteReview) throws ReviewDeleteException {
+        int result = bookingMapper.reviewDelete(deleteReview);
+
+        if(!(result > 0)) {
+            throw new ReviewDeleteException("후기 삭제에 실패하였습니다");
+        }
+    }
 }
---
+++ src/main/java/com/metavirtual/bloom/common/exception/booking/ReviewDeleteException.java
@@ -0,0 +1,9 @@
+package com.metavirtual.bloom.common.exception.booking;
+
+public class ReviewDeleteException extends Exception {
+
+    public ReviewDeleteException() {}
+    public ReviewDeleteException(String message){
+        super(message);
+    }
+}
